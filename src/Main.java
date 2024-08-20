07da147a293045d5cf491fd76634aa74c6abbea4
vinileoml <viniciusleomoraisdelima@gmail.com>
2024-08-19 21:35:21 -0300

    Comit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Rendrik");
+        //TipoDaVariavel nomeDaVariavel = ValorDaVariavel
+        //String é um texto
+        String nome="Rendrik";
+        char sexo = 'M';
+
+        //[] -> Diz que é um array
+        //{} -> Insicar um objeto
+
+        char[] textoChar = {'5'};
+
+        //Guardar valores reais
+
+        double altura = 1.76;
+        Double peso = 72.0;
+        float alturapessoa2 = 1.76f;
+        Float alturapessoa3 = 1.72f;
+        int numeroCasa = 122;
+        Integer numeroAlunos = 75;
+        Long numeroGolsDoBraitWaith = 3l;
+
+        //Operadores aritmeticos
+        // + para soma
+        // - para subtração
+        // * para multiplicação
+        // / para divisão
+
+        Integer soma = 5 + 5;
+        Integer subtração = 5 - 2;
+        Integer multiplicar = 5 * 5;
+        Integer dividir = 5 / 5;
+
+       Integer resultadoSoma = soma + subtração;
+       Integer resultadoSubtracao = soma - subtração;
+       Integer resultadoMultiplicar = soma * subtração;
+       Integer resultadoDivisao = soma / subtração;
+
+       System.out.println("valor da divisao é : " + resultadoDivisao);
+       System.out.println("valor da multiplicacao é : " + resultadoMultiplicar);
+       System.out.println("valor da soma é : " + resultadoSoma);
+       System.out.println("valor da subtração é : " + resultadoSubtracao);
+
+
+
+
+
+
+
+    }
+}
